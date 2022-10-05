930f839b743bbf45720c1f3ed3246a753f1cb3e2
Akeen98 <akeenbandara123@gmail.com>
2022-10-05 11:10:52 +0530

    java comments

---
+++ java-practice/comment.java
@@ -0,0 +1,8 @@
+class comment{
+    public static void  main(String[]args){
+        System.out.print("Sasith modaya"); // use the comments usings two forward slashes
+
+       /* use the multi line comments using the forward slashes and star*/
+        
+    }
+}
